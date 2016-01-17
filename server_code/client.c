0009fc460938af717b9966dcbfe585a95c60a623
jerrylei98 <jerrylei98@gmail.com>
2016-01-16 23:05:54 -0500

    modified to work on 104.131.83.105

--- server_code/client.c
+++ server_code/client.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <errno.h>
 
 //needed for server/clients (new header files)                                                                                                           
 #include <sys/types.h>
@@ -17,21 +17,21 @@ int main(int argc, char **argv){
   int i;
 
   //create socket
-  socket_id=socket(AF_INET, SOCK_STREAM);
+  socket_id=socket(AF_INET, SOCK_STREAM,0);
   
   //bind to port/address
   struct sockaddr_in sock;
   sock.sin_family = AF_INET;
   sock.sin_port = htons(5000);
   
-  inet_aton("127.0.0.1", &(sock.sin_addr));
+  inet_aton("104.131.83.105", &(sock.sin_addr));
   bind(socket_id, (struct sockaddr *)&sock, sizeof(sock));
 
   //attempt a connection
   i = connect(socket_id, (struct sockaddr *) &sock, sizeof(sock));
   printf("<client> connect returned %d\n", i);
   if(i == -1){
-    printf("<client> error: %d %s\n", errno, strenno(errno));
+    // printf("<client> error: %d %s\n", errno, strenno(errno));
     exit(0);
   }
   read(socket_id, buffer, sizeof(buffer));
@@ -39,3 +39,4 @@ int main(int argc, char **argv){
   close(socket_id);
     
   
+}
