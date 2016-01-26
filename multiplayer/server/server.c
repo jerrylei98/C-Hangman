9b0fde8b614223f11502eb6780f8a1b0f62937dc
softdev <jerrylei98@gmail.com>
2016-01-25 23:04:00 -0500

    scrapping client/server

--- multiplayer/server/server.c
+++ multiplayer/server/server.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
-//needed for server/clients (new header files)
 #include <sys/types.h> 
 #include <sys/socket.h>  
 #include <netinet/in.h>
@@ -27,14 +25,18 @@ int main(){
   listen(socket_id, 1);
   printf("<server listening>\n");
 
+  while(1){
+    if(game_set.current_hangman < 7){
+      socket_client = accept(socket_id, NULL, NULL);
+      printf("<server> connected: %d\n", socket_client);
   
-  socket_client = accept(socket_id, NULL, NULL);
-  printf("<server> connected: %d\n", socket_client);
-  
-  write(socket_client, "hello", 6);
-  
-  close(socket_client);
-  close(socket_id);
-    
+      write(socket_client, print_game(), sizeof(print_game()));
+    }
+    else if(game_set.current_hangman > 6){
+
+      close(socket_client);
+      close(socket_id);
+    }
+  }
   return 0;
 }
